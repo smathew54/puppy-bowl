dc7a186106ce76f2c42845ed1233e2d0b9c23f59
smathew54 <88407507+smathew54@users.noreply.github.com>
2025-07-10 19:36:14 -0500

    this is the starter code

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
 // === Constants ===
 const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
+const COHORT = "/2506-Shawn"; // Make sure to change this!
 const API = BASE + COHORT;
+
+//2505-ftb-ct-web-pt-Shawn
+// === State ===
+let puppyList = [];
+let selectedPuppy = []
+
+
+let rsvp = [];
+let guests = [];
+
+/** Updates state with all the puppies from the API */
+async function getPuppies() {
+    try {
+        const apiURL = API +"/players" //double check the endpoint
+        console.log(apiURL)
+        const response = await fetch(apiURL)
+        const result = await response.json()
+        const players = result.data;
+        console.log(players);
+        render()
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+
+async function getPuppy(id) {
+    try {
+        const apiURL = API + "/players/" + id;
+        console.log(apiURL);
+        const response = await fetch(apiURL);
+        const result = await response.json();
+        const individualPuppy = result.data
+        console.log(individualPuppy)
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+
+//don't know if we need a third function
+//async function getRsvps() {
+//    try
+//}
+
+// === Components ===
+function Puppy
+
+
+// === Render ===
+function render() {
+    const $app = document.querySelector("#app");
+    $app.innerHTML = `
+    <h1>Puppy Party</h1>
+    `
+}
+
+//getPuppy("39006")
+
+async function main() {
+    console.log("HERE WE GO");
+    await getPuppies();
+    render()
+}
+
+main()
