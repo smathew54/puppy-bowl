a82f45aeb6b3eaef6533bad41148a0856a822a96
smathew54 <88407507+smathew54@users.noreply.github.com>
2025-07-10 20:16:08 -0500

    have the initial form and some of the headers

--- index.js
+++ index.js
@@ -48,15 +48,131 @@ async function getPuppy(id) {
 //}
 
 // === Components ===
-function Puppy
+function PuppyListItem(roster) {
+    const $li = document.createElement("li");
 
+    if (roster.id === selectedPuppy?.id) {
+        $li.classList.add("selected");
+    }
+
+
+
+}
+
+//addParty
+const addParty = async (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target[0]);
+    console.log(e.target[0].value);
+    console.log(e.target[1].value);
+    console.log(e.target[2].value);
+    console.log(e.target[3].value);
+  
+    const isoDate = new Date(e.target[2].value).toISOString()
+  
+    const obj = {
+      name: e.target[0].value,
+      description: e.target[1].value,
+      date: isoDate,
+      location: e.target[3].value,
+    };
+  
+    console.log(obj);
+    try {
+      const response = await fetch(API + "/events",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(obj),
+        }
+      );
+  
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  ;
+
+
+//add puppy to the list
+const setForm = () => {
+    const $form = document.createElement("form");
+    $form.innerHTML = `
+        <div class="form-group">
+          <label for="enterPartyName">Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="enterPartyName"
+            aria-describedby="emailHelp"
+            placeholder="Enter Party Name"
+          />
+        </div>
+        <div class="form-group">
+          <label for="dogBreed">Breed</label>
+          <input
+            type="test"
+            class="form-control"
+            id="dogBreed"
+            placeholder="Breed"
+          />
+        </div>
+        <div class="form-group">
+          <label for="status">Status</label>
+          <input
+            type="test"
+            class="form-control"
+            id="status"
+            placeholder="Status"
+          />
+        </div>
+
+        </div>
+        <div class="form-group">
+          <label for="imageURL">Image URL</label>
+          <input
+            type="text"
+            class="form-control"
+            aria-describedby="emailHelp"
+            placeholder="imageURL"
+          />
+        </div>
+  
+        <button type="submit" class="btn btn-primary">Submit</button>
+      `;
+    $form.style.width = "75%";
+    $form.style.margin = "0 auto";
+  
+    $form.addEventListener("submit", addParty)
+  
+    return $form;
+  };
 
 // === Render ===
 function render() {
     const $app = document.querySelector("#app");
     $app.innerHTML = `
     <h1>Puppy Party</h1>
-    `
+    <main>
+        <section>
+          <h2>Puppy Player Options</h2>
+          <Puppies></Puppies>
+        </section>
+        <section>
+          <h2>Invite a new Puppy</h2>
+          <PuppyForm></PuppyForm>
+        <section>
+    </main>
+    `;
+
+    $app.querySelector("PuppyForm").replaceWith(setForm());
 }
 
 //getPuppy("39006")
